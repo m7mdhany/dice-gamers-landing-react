90408e27e7ace43dbd0e7f34f64924b46e035702
m7mdhany <muhamedhany.m@gmail.com>
2025-10-17 19:45:52 +0300

    style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='md:h-96 md:my-10'>
       <div className='h-full md:flex hidden justify-center items-center gap-5 md:px-20'>
         <div className='flex flex-col items-end w-1/2 gap-10'>
-          <h1 className='text-end font-bold self-end text-2xl lg:text-7xl'>DICE GAMERS</h1>
+          <h1 className='w-full text-end font-bold text-2xl lg:text-7xl'>DICE GAMERS</h1>
           <p className=''>
             وجهتك المثالية لألعاب الطاولة المصنوعة يدويًا بجودة عالية. سواء كنت من عشاق التحديات الاستراتيجية، المغامرات المثيرة، أو الأوقات العائلية المرحة، لدينا ما يناسبك تمامًا. اكتشف مجموعتنا المميزة من الألعاب المصنوعة من الخشب والأكريليك، المصممة خصيصًا لرفع مستوى تجربتك في اللعب.
           </p>
